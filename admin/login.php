3e0972d199899bc82f8667e41aa53c6808b5f83a
Alinurrr <alinoer.rachman@gmail.com>
2020-07-09 14:32:40 +0700

    memberikan validasi form login dan register

--- admin/login.php
+++ admin/login.php
@@ -3,25 +3,58 @@ include "../lib/koneksi.php";
 $email = $_POST['Email'];
 $password = $_POST['Password'];
 
-$login = mysqli_query($koneksi, "SELECT * FROM tb_user WHERE email='$email' AND password = '$password'");
-$ketemu = mysqli_num_rows($login);
-$r = mysqli_fetch_array($login);
+
 
 // apabila Email dan password ditemukan 
-if ($ketemu > 0) {
-    session_start();
-    $_SESSION['email'] = $r['email'];
-    $_SESSION['password'] = $r['password'];
+if (empty($email)) {
+    echo "<center><script> alert('email kosong!')</script>";
+    echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";
+            echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<b>email anda kosong!</b><br>";
+            echo "silahkan klik dibawah ini untuk login kembali<br>";
+            echo "<br>";echo "<a href=index.php><b>ULANGI LAGI</b></a></center>";
+} else if (empty($password)){
+    echo "<center><script> alert('password kosong!')</script>";
+    echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";
+            echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<b>password anda kosong!</b><br>";
+            echo "silahkan klik dibawah ini untuk login kembali<br>";
+            echo "<br>";echo "<a href=index.php><b>ULANGI LAGI</b></a></center>";
+} else if ($password == " "){
+    echo "<center><script> alert('Karakter spasi tidak diperbolehkan')</script>";
+    echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";
+            echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<b>password anda kosong!</b><br>";
+            echo "silahkan klik dibawah ini untuk login kembali<br>";
+            echo "<br>";echo "<a href=index.php><b>ULANGI LAGI</b></a></center>";
+} else if ($password == "  "){
+    echo "<center><script> alert('Karakter spasi tidak diperbolehkan')</script>";
+    echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<br>";
+            echo "<br>";echo "<br>";echo "<br>";echo "<br>";echo "<b>password anda kosong!</b><br>";
+            echo "silahkan klik dibawah ini untuk login kembali<br>";
+            echo "<br>";echo "<a href=index.php><b>ULANGI LAGI</b></a></center>";
+}   else{
 
-    $email = $_SESSION['email'];
-    $queryAdmin = mysqli_query($koneksi, "SELECT * FROM tb_user WHERE email='$email'");
-    $hasilQuery = mysqli_fetch_array($queryAdmin);
-    $status = $hasilQuery['status'];
+            $login = mysqli_query($koneksi, "SELECT * FROM tb_user WHERE email='$email' AND password = '$password'");
+            $ketemu = mysqli_num_rows($login);
+            $r = mysqli_fetch_array($login);
 
-    header('Location: ../index.php');
-} else {
-    echo "<center>LOGIN GAGAL! <br>
-        Email atau password anda tidak benar. <br>
-        atau akun anda sedang di blokir <br>";
-    echo "<a href=index.php><b>ULANGI LAGI</b></a></center>";
+    // apabila username dan password ditemukan 
+            if ($ketemu > 0) {
+                session_start();
+                $_SESSION['email'] = $r['email'];
+                $_SESSION['password'] = $r['password'];
+
+                $email = $_SESSION['email'];
+                $queryAdmin = mysqli_query($koneksi, "SELECT * FROM tb_user WHERE email='$email'");
+                $hasilQuery = mysqli_fetch_array($queryAdmin);
+                $status = $hasilQuery['status'];
+
+
+                    header('Location: ../index.php');
+
+            } else {
+                echo "<center>LOGIN GAGAL! <br>
+                Email atau password anda tidak benar. <br>
+                atau akun anda sedang di blokir <br>";
+                echo "<a href=index.php><b>ULANGI LAGI</b></a></center>";
+            }
+
 }
--- admin/module/register/simpan_register.php
+++ admin/module/register/simpan_register.php
@@ -7,12 +7,40 @@ $email = $_POST['Email'];
 $password = $_POST['Password'];
 $status = $_POST['Status'];
 
+if (empty($nama_pengguna)) {
+    echo "<center><script> alert('nama kosong!'); window.location='$admin_url'+'register.php';</script>";
 
-$querySimpan = mysqli_query($koneksi, "INSERT INTO tb_user (nama, email, password, status) VALUES 
+} else if ($nama_pengguna ==" ") {
+    echo "<center><script> alert('nama dengan karakter spasi tidak diperbolehkan!'); window.location='$admin_url'+'register.php';</script>";
+
+}  else if ($nama_pengguna ==" ") {
+    echo "<center><script> alert('nama dengan karakter spasi tidak diperbolehkan!'); window.location='$admin_url'+'register.php';</script>";
+
+} else if (empty($email)) {
+    echo "<center><script> alert('email kosong!'); window.location='$admin_url'+'register.php';</script>";
+
+} else if (empty($password)){
+    echo "<center><script> alert('password kosong!'); window.location='$admin_url'+'register.php';</script>";
+
+} else if ($password == " "){
+    echo "<center><script> alert('password dengan karakter spasi tidak diperbolehkan!'); window.location='$admin_url'+'register.php';</script>";
+
+} else if ($password == "  "){
+    echo "<center><script> alert('password dengan karakter spasi tidak diperbolehkan!'); window.location='$admin_url'+'register.php';</script>";
+
+} else if (empty($status)){
+    echo "<center><script> alert('status kosong!'); window.location='$admin_url'+'register.php';</script>";
+
+}else{
+
+		$querySimpan = mysqli_query($koneksi, "INSERT INTO tb_user (nama, email, password, status) VALUES 
                                                   ('$nama_pengguna','$email','$password','$status')");
 
-if ($querySimpan) {
-    echo "<script> alert('Data member Berhasil disimpan silahkan login untuk masuk '); window.location='$admin_url'+'index.php';</script>";
-} else {
-    echo "<script> alert('Data Member Gagal Dimasukkan'); window.location='$admin_url'+'register.php'; </script>";
+		if ($querySimpan) {
+		    echo "<script> alert('Data member Berhasil disimpan silahkan login untuk masuk '); window.location='$admin_url'+'index.php';</script>";
+		} else {
+    		echo "<script> alert('Data Member Gagal Dimasukkan'); window.location='$admin_url'+'register.php'; </script>";
+		}
+
+
 }
